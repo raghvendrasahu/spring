51cc1ad1b6c13a65beb1e1bb92c03245b3cd3ab1
Raghvendra <ragh@structure101.com>
2018-12-01 17:34:36 +0530

    initial

---
+++ src/main/java/com/raghavx/raghavx/RaghavxApplication.java
@@ -0,0 +1,12 @@
+package com.raghavx.raghavx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaghavxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaghavxApplication.class, args);
+	}
+}
